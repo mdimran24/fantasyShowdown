c9101fc3a97bd27b1b3494616f5678dcc7a76a0b
Crina Zetu <200023627@aston.ac.uk>
2021-10-28 11:28:40 +0100

    extra assets

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+
+    public GameObject card;
+    public GameObject playerArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
