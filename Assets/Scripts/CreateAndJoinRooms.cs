a0e111d07d0322692ec6fd524b93d617293c2348
Crina Zetu <200023627@aston.ac.uk>
2021-11-03 13:32:32 +0000

    Added basic multiplayer functionality, now we need to actually make the game work multiplayer

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class CreateAndJoinRooms : MonoBehaviour
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public InputField createInput;
+  public InputField joinInput;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public void CreateRoom(){
+      PhotonNetwork.CreateRoom(createInput.text);
+  }
+
+  public void JoinRoom(){
+      PhotonNetwork.JoinRoom(joinInput.text);
+  }
+
+  public override void OnJoinedRoom(){
+      PhotonNetwork.LoadLevel("MainGame");
+  }
 }
