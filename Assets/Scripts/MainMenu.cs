21c2d7302e52fcf19819402267ad993a82405166
Crina Zetu <200023627@aston.ac.uk>
2021-11-03 10:05:07 +0000

    In the process of making the lobby

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,10 @@ public class MainMenu : MonoBehaviour
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
 
+   public void PlayDemo(){
+     SceneManager.LoadScene("Loading");
+   }
+
    public void QuitGame(){
      Debug.Log("QUIT!");
      Application.Quit();
