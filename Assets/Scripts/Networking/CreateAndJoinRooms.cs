6053cb26fde023ca60ef818b543107b463501298
Crina Zetu <200023627@aston.ac.uk>
2021-12-10 22:19:11 +0000

    pushing this before I break something

---
+++ Assets/Scripts/Networking/CreateAndJoinRooms.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public InputField create;
+    public InputField join;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(create.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(join.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MultiplayerGame");
+    }
+}
