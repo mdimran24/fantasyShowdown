e174a6f97794b86c92e25576b9839e76df66ebe4
Mohammed Imran <58631258+mdimran24@users.noreply.github.com>
2022-02-06 12:18:42 +0000

    Crossfade animation added, forestwipe reimplemented

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    public string sceneName;
+
+
+    public void Start()
+    {
+        StartCoroutine(LoadLevel());
+    }
+    public void loadscene(string scenename)
+    {
+
+        StartCoroutine(LoadLevel(scenename));
+    }
+
+    IEnumerator LoadLevel()
+    {
+        //Play Animation
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        //SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        //Play Animation
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
